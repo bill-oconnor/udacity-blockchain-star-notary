10e55afa3bd1e98bd11135f529f084e5a3051585
William O'Connor <williamboconnor@gmail.com>
2021-07-08 23:38:19 -0500

    Fixed the web3 issues by installing 1.3

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import Web3 from "web3";
 import starNotaryArtifact from "../../build/contracts/StarNotary.json";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 const App = {
   web3: null,
@@ -10,12 +11,11 @@ const App = {
     console.log("start!");
     const { web3 } = this;
     console.log({web3});
-    window.web3 = web3;
 
     try {
       console.log("start trying!");
       // get contract instance
-      const networkId = await new Promise(resolve => this.web3.eth.net.getId(resolve));
+      const networkId = 4;//await window.web3.eth.net.getId();
       console.log({networkId});
       const deployedNetwork = starNotaryArtifact.networks[networkId];
       console.log({deployedNetwork});
@@ -24,9 +24,10 @@ const App = {
         deployedNetwork.address,
       );
       console.log({meta: this.meta});
+      window.Contract = this.meta;
 
       // get accounts
-      const accounts = await web3.eth.getAccounts();
+      const accounts = (await ethereum.request({ method: "eth_requestAccounts" }));
       console.log(accounts);
       this.account = accounts[0];
     } catch (error) {
@@ -51,9 +52,10 @@ const App = {
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
     const { lookUptokenIdToStarInfo } = this.meta.methods;
-    const starID = document.getElementById("lookid");
+    const starIDInput = document.getElementById("lookid");
+    const starID = starIDInput && starIDInput.value;
     if (starID) {
-      const starName = await lookUptokenIdToStarInfo(starID);
+      const starName = await lookUptokenIdToStarInfo(starID).call();
       App.setStatus(`The name of the star with id=${starID} is ${starName}`);
     }
   }
@@ -63,12 +65,18 @@ const App = {
 window.App = App;
 
 window.addEventListener("load", async function() {
-  if (window.ethereum) {
+  const provider = await detectEthereumProvider();
+  if (provider) {
     // use MetaMask's provider
     console.log("window.ethereum checks out");
-    App.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable(); // get permission to access accounts
+    const web3 = new Web3(provider);
+    window.web3 = web3;
+    App.web3 = web3;
+    const accounts = await provider.enable(); // get permission to access accounts
     console.log("done enabling");
+    const chainId = await ethereum.request({ method: 'eth_chainId' });
+    const validConnection = accounts.length > 0;
+    console.log({validConnection, chainId});
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
